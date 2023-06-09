a8472d09eab32cceb621f3fb050d93477ad3f6b4
ibrahimbank <bankoleibrahim96@gmail.com>
2023-06-09 01:49:05 +0100

    Added more options

--- src/ussd/ussd.controller.ts
+++ src/ussd/ussd.controller.ts
@@ -20,11 +20,35 @@ export class UssdController {
       case '':
         return (
           'CON What would you want to check \n' +
-          '1. My Account \n' +
-          '2. My phone number'
+          '1. My Wallet Balance \n' +
+          '2. My HMO \n' +
+          '3. Phone number \n' +
+          '4. My Id \n' +
+          '5. HMO Co-Pay \n' +
+          '6. Service charge \n'
         );
+      case '1*1':
+        return (
+          'CON Which Balance do you want to check \n' +
+          '1. Pfizer Pharmaceuticals\n' +
+          '2. Roche Product Limited \n'
+        );
+      case '1*1*1':
+        return 'End Your Pfizer Pharmaceuticals Balance is 498,899.00';
+      case '1*1*2':
+        return 'End Roche Product Limited Balance is 790,760.00';
+      case '1*2':
+        return 'End Your HMO is Health Care International LTD';
+      case '1*3':
+        return 'End Your Phone Number is +234090898534';
+      case '1*4':
+        return 'End Your user ID is UNT0203581';
+      case '1*5':
+        return 'End Your HMO Co-Pay is ₦1,001,011.50';
+      case '1*6':
+        return 'End Your Service charge is ₦30,000.00';
       default:
-        return 'Thanks for using stuff';
+        return 'End Thanks for using NHIS Service';
     }
   }
 }
