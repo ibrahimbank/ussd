ac6fe9803ad8e66fbd6a04dde1ea561876350699
ibrahimbank <bankoleibrahim96@gmail.com>
2023-06-09 11:48:18 +0100

    checking new dev

--- src/ussd/ussd.controller.ts
+++ src/ussd/ussd.controller.ts
@@ -80,6 +80,37 @@ export class UssdController {
       case '2*6':
         return 'END Your Service charge is ₦30,000.00';
 
+      case '3':
+        return (
+          'CON 1SP \n' +
+          '1. My Wallet Balance \n' +
+          '2. My HMO \n' +
+          '3. Phone number \n' +
+          '4. My Id \n' +
+          '5. HMO Co-Pay \n' +
+          '6. Service charge \n'
+        );
+      case '3*1':
+        return (
+          'CON Which Balance do you want to check \n' +
+          '1. Pfizer Pharmaceuticals\n' +
+          '2. Roche Product Limited \n'
+        );
+      case '3*1*1':
+        return 'END Your Pfizer Pharmaceuticals Balance is 498,899.00';
+      case '3*1*2':
+        return 'END Roche Product Limited Balance is 790,760.00';
+      case '3*2':
+        return 'END Your HMO is Health Care International LTD';
+      case '3*3':
+        return 'END Your Phone Number is +234090898534';
+      case '3*4':
+        return 'END Your user ID is UNT0203581';
+      case '3*5':
+        return 'END Your HMO Co-Pay is ₦1,001,011.50';
+      case '3*6':
+        return 'END Your Service charge is ₦30,000.00';
+
       case 'default':
         return 'END Invalid input';
       default:
